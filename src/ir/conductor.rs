1febd3ef2b7f2f0a76dbddc011a224b51342a700
LER0ever <i@rongyi.io>
2020-03-19 15:20:02 +0800

    feat(ir/conductor): attempt to add unified api for planning
    Signed-off-by: LER0ever <i@rongyi.io>

---
+++ src/ir/conductor.rs
@@ -0,0 +1,26 @@
+use crate::input::{hlo_string::HLOStructuredJsonImporter, HLOModelImporter};
+use crate::ir::derive::Derivation;
+use crate::ir::hlo_ast::HLORoot;
+use pyo3::exceptions;
+use pyo3::prelude::*;
+use std::error::Error;
+
+pub struct IRConductor {
+    pub ast: HLORoot,
+}
+
+impl IRConductor {
+    /// Construct a new IRConductor for working with HLO/MLIR model planning
+    pub fn new() -> Self {
+        IRConductor {
+            ast: HLORoot { functions: vec![] },
+        }
+    }
+
+    /// Import the model information from `filename`
+    pub fn import_from(&mut self, filename: String) -> Result<(), Box<dyn Error>> {
+        let hlo_importer: HLOStructuredJsonImporter = HLOModelImporter::new();
+        self.ast = hlo_importer.ImportFrom(filename.as_str())?;
+        Ok(())
+    }
+}
