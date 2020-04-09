3cb1cc4637e0875bc9552abec121012972c70707
LER0ever <i@rongyi.io>
2020-04-09 14:26:55 +0800

    feat(ir/propagate): use inst_id + color to uniquely identify result
    Signed-off-by: LER0ever <i@rongyi.io>

--- src/ir/propagate/ast_propagate.rs
+++ src/ir/propagate/ast_propagate.rs
@@ -30,10 +30,13 @@ impl Context {
         unimplemented!()
     }
 
+    /// Propagate a given function with a determined variable split and constraint
+    /// map. Returns a new constraint map if succeeded, and None if conflict. Errors
+    /// are propagated gracefully.
     pub fn propagate_bfs(
         &self,
         func_id: usize,
-        p_name: &str, // NOTE: only pass
+        p_name: &str, // NOTE: only pass parameter here, preferably non-repetitive
         split: i8,
         constraints: &HashMap<&str, HashSet<i8>>,
     ) -> Result<Option<(HashMap<&str, HashSet<i8>>)>, Box<dyn Error>> {
@@ -42,10 +45,13 @@ impl Context {
         // make a copy of constraint map, pending return.
         let m = constraints.clone();
         // NOTE: check compliance with constraint
+        if m.contains_key(p_name) && m[p_name].contains(&split) {
+            return Ok(None); // conflict with constraint
+        }
         assert_eq!(!m.contains_key(p_name) || m[p_name].contains(&split), true);
 
         let f = &self.ast.functions[func_id];
-        let v_inst_color: HashMap<usize, HashSet<usize>> = HashMap::new();
+        let mut v_inst_color: HashMap<usize, HashSet<usize>> = HashMap::new();
         let v_var_split: HashMap<&str, HashSet<i8>> = HashMap::new();
         assert_eq!(self.ast.var_pos[p_name].0, func_id);
 
@@ -55,8 +61,24 @@ impl Context {
         while !q.is_empty() {
             let (v, s) = q.pop_front().unwrap();
             let v_pos = &self.ast.var_pos[v].1;
+            // iterate over all positions of the variable
             for vp in v_pos {
+                let v_derive: Vec<(HashMap<String, i8>, usize)> =
+                    self.derive(func_id, *vp, v, s)?;
+                for (d, i) in v_derive {
+                    // NOTE: check if we've visited the same inst at the same color
+                    if v_inst_color.contains_key(vp) && v_inst_color[vp].contains(&i) {
+                        continue;
+                    } else if v_inst_color.contains_key(vp) {
+                        v_inst_color.get_mut(vp).unwrap().insert(i);
+                    } else {
+                        v_inst_color.insert(*vp, [i].iter().cloned().collect());
+                    }
+                    // TODO: check the above logic
 
+
+
+                }
             }
         }
 
@@ -69,14 +91,15 @@ impl Context {
         inst_id: usize,
         var_name: &str,
         split: i8,
-    ) -> Result<Vec<HashMap<String, i8>>, Box<dyn Error>> {
+    ) -> Result<Vec<(HashMap<String, i8>, usize)>, Box<dyn Error>> {
         let inst_derive = Derivation::derive(&self.derive, func_id, inst_id)?;
-        let mut result: Vec<HashMap<String, i8>> = vec![];
-        for d in inst_derive {
+        let mut result: Vec<(HashMap<String, i8>, usize)> = vec![];
+        inst_derive.iter().enumerate().for_each(|(i, d)| {
             if d.contains_key(var_name) && d[var_name] == split {
-                result.push(d.clone());
+                // NOTE: we depend on the order of solution being constant here
+                result.push((d.clone(), i));
             }
-        }
+        });
         Ok(result)
     }
 }
