634eb782bba81d62adf745007dd6a5c5f2435609
LER0ever <i@rongyi.io>
2020-04-21 12:37:48 +0800

    feat(pruning): add separate pruning precedure, add prune example
    Signed-off-by: LER0ever <i@rongyi.io>

---
+++ src/ir/prune/param_priority.rs
@@ -0,0 +1,7 @@
+use crate::ir::propagate::ast_propagate::*;
+use crate::ir::hlo_ast::{HLORoot, Param};
+use rayon::prelude::*;
+
+pub fn prioritized_params(ctx: &Context, func_id: usize, params: Vec<Param>) -> Vec<Param> {
+    unimplemented!()
+}
