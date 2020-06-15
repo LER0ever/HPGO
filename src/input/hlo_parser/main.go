13dc9734e4dc0c507d5067ce0d171da083b016fb
LER0ever <i@rongyi.io>
2020-06-15 12:08:27 +0800

    fix(hlo-parser): change argument.value back to argument.name, previous change was unnecessary
    Signed-off-by: LER0ever <i@rongyi.io>

--- src/input/hlo_parser/main.go
+++ src/input/hlo_parser/main.go
@@ -99,8 +99,8 @@ type Param struct {
 }
 
 type Argument struct {
-	Type  Type   `(@@)?`
-	Value *Value `@@`
+	Type Type   `(@@)?`
+	Name *Value `@@`
 	//Name string `@VarName | @Number | @Ident`
 }
 
